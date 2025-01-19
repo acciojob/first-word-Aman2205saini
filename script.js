a9d2ebcb12a66c6dc0010e3b0bafaa140933665c
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-19 20:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	 s = s.trim();
+    
+    // Find the index of the first space
+    let spaceIndex = s.indexOf(' ');
+    
+    // If no space is found, return the entire string, otherwise return the substring up to the first space
+    if (spaceIndex === -1) {
+        return s; // No space found, return the entire string
+    } else {
+        return s.substring(0, spaceIndex); // Return the substring from the start to the first space
+    }
 }
 
 // Do not change the code below
